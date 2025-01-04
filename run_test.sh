585508c010d360cc1f5b9fc1b858b52d33e7ceb2
aerijman <aerijman@neb.com>
2025-01-03 19:49:02 -0500

    WIP

--- run_test.sh
+++ run_test.sh
@@ -27,6 +27,12 @@ else
 fi
 . ${activate_conda} || echo "Perhaps conda is not in: $(type -a conda) ?"
 
+conda update conda -y
+conda update --all -y
+conda config --add channels defaults 
+conda config --add channels conda-forge 
+conda config --add channels bioconda 
+conda install -c conda-forge libgcc-ng=12 -y
 
 conda create --name nextflow.emseq --yes python=3.8 && conda install --name nextflow.emseq --yes bioconda:nextflow=23.10 bioconda::samtools=1.19
 conda activate nextflow.emseq
