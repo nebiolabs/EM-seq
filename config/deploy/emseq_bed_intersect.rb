d4e46e79d88547cb15651691f55ef23ba0eabc85
Laura Blum <lblum@neb.com>
2025-08-27 10:36:31 -0400

    plan to deploy with seq-shepherd as submodule to simplify

--- config/deploy/emseq_bed_intersect.rb
+++
@@ -1,7 +0,0 @@
-set :application, "EM-seq"
-set :repo_url, "git@github.com:nebiolabs/EM-seq.git"
-set :branch, "bed_intersect"
-set :deploy_to, "#{ENV['DEPLOY_PATH']}/bed_intersect"
-
-role :app, ["#{ENV['DEPLOY_USER']}@#{ENV['DEPLOY_HOST']}"]"
-append :linked_files, "nextflow.config"
