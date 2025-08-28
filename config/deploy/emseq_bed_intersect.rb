079c354c6d6f31b2cced9bb577be79044f3e7dca
aerijman <aerijman@neb.com>
2025-08-28 10:10:53 -0400

    extra quote

--- config/deploy/emseq_bed_intersect.rb
+++ config/deploy/emseq_bed_intersect.rb
@@ -3,5 +3,5 @@ set :repo_url, "git@github.com:nebiolabs/EM-seq.git"
 set :branch, "bed_intersect"
 set :deploy_to, "#{ENV['DEPLOY_PATH']}/bed_intersect"
 
-role :app, ["#{ENV['DEPLOY_USER']}@#{ENV['DEPLOY_HOST']}"]"
+role :app, ["#{ENV['DEPLOY_USER']}@#{ENV['DEPLOY_HOST']}"]
 append :linked_files, "nextflow.config"
