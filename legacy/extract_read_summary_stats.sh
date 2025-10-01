438ab346d6ca5806be7356a2939b908d1b2d6cb4
Brad Langhorst <langhorst@neb.com>
2025-10-01 18:15:21 -0400

    usability improvements for summary stats

--- legacy/extract_read_summary_stats.sh
+++ legacy/extract_read_summary_stats.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# this program extracts field sfrom  samtools flagstat to a tabular file suitable for tableau reporting
+# this program extracts fields from samtools flagstat to a tabular file suitable for tableau reporting
+# expects a list of bam files to summarize as arguments
 
 #9791    173     total (QC-passed reads + QC-failed reads)
 #9787    151     primary
@@ -28,9 +29,11 @@ echo -e 'file\tprimary_reads\tprimary_duplicates\tprimary_mapped\tusable_reads'
 for f in "$@"; do 
 
   echo -ne "$f\t"
-  samtools flagstat -O tsv $f | grep -E -e 'primary$' -e 'primary duplicates$' -e 'primary mapped$' \
+  [[ ! -f "${f}.flagstat" ]] && samtools flagstat -O tsv "$f" 1> ${f}.flagstat 2> "$f.flagstat.err"
+  cat "$f.flagstat" | grep -E -e 'primary$' -e 'primary duplicates$' -e 'primary mapped$' \
     | awk -v FS='\t' -v OFS='\t' '{print $1+$2}' | paste - - - \
     | tr '\n' '\t'
-  samtools view -F 0xF00 -q 10 -c $f 
+  [[ ! -f "${f}.usable_reads" ]] && samtools view -F 0xF00 -q 10 -c "$f" > "${f}.usable_reads" 2> "${f}.usable_reads.err"
+  cat "${f}.usable_reads"
 done 
 
